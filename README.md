a9feee815e56a65618e9383afff3d98cf9951951
Joseph Haaga <haaga.joe@gmail.com>
2020-10-31 22:29:30 -0400

    resolving entities w/ Wikidata

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Constructing a Spacy KnowledgeBase by reading news articles and referencing Wiki
 pip install -r requirements.txt
 python3 main.py
 ```
+
+
+# TODO: embed descriptions and predict the best match (similarity to article text)
+# TODO: extract facts from article related to entity via dependency parser
+
--- main.py
+++ main.py
@@ -1,35 +1,76 @@
 from typing import Dict
 from typing import List
+from typing import Sequence
 
 from bs4 import BeautifulSoup
 from newspaper import Article
 import requests
 import spacy
+from spacy import displacy
 
+
+nlp = spacy.load("en_core_web_md")
 
 def main() -> int:
     # download an article
     URL = "https://www.cnbc.com/2020/09/22/palantir-says-it-expects-42percent-revenue-growth-this-year-to-1point06-billion.html"
-    article = Article(URL)
-    article.download()
-    article.parse()
-    print(f"Processing article: \"{article.title}\"")
+    article = download_article(URL)
+    doc = nlp(article.text)
+    doc.user_data["title"] = article.title
+    # initial = displacy.serve(doc, style="ent")
 
     # process the article
-    nlp = spacy.load("en_core_web_md")
-    parsed = nlp(article.text)
-    orgs = {o.text for o in parsed.ents if o.label_ == "ORG"}
-    print(f"Extracted {len(orgs)} ORG entities")
-    print(orgs)
+    entities = [o for o in doc.ents if o.label_ in ["ORG", "PERSON", "GPE", "WORK_OF_ART", "FAC"]]
+    print(f"Extracted {len(entities)} entities")
 
-    # look for entities on wikidata
+
+    # TODO: Consolidate multiple mentions of each entity into one (multiple sentences reference Palantir, but they resolve to different Wikidata entities)
+    for entity in entities:
+        # TODO: skip if already added to KnowledgeBase on an earlier iteration
+        mention = get_mention_of_entity(entity)
+        possible_matches = search_wikidata(entity.text)
+        if len(possible_matches) == 1:
+            match = possible_matches[0]
+            print(f"{entity.text} is actually {match['id']} - {match['title']}: {match['description']}")
+        elif len(possible_matches) > 1:
+            #mention_doc = nlp(mention)
+            x = {}
+            for match in possible_matches:
+                x[match["id"]] = nlp(match["description"]).similarity(mention)
+                # TODO: embed the 
+            top_result = sorted(list(x.items()), key=lambda x: x[1], reverse=True)[0]
+            best_match, = [match for match in possible_matches if match["id"] == top_result[0]]
+            print(f"{entity.text} is likely {best_match['id']} - {best_match['title']}: {best_match['description']} with confidence {top_result[1]}")
+            breakpoint()
+        else:
+            print(f"Wikidata has no match for {entity.text}")
+        # TODO: add to KnowledgeBase
+    breakpoint()
+    # TODO: retrain the model/KnowledgeBase
+
+
+    # resolve to entities on wikidata
     for org in orgs:
         wikidata_matches = search_wikidata(org)
         print(f"{org} returned {len(wikidata_matches)} Wikidata matches:")
         for match in wikidata_matches:
             print(f"  {match['id']} - {match['title']}: {match['description']}")
-        # TODO: embed descriptions and predict the best match (similarity to article text)
 
+
+def download_article(url: str) -> Article:
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article
+
+
+def get_mention_of_entity(entity):
+    sentences = list(entity.doc.sents)
+    i = 0
+    for ind, length in enumerate([len(sent.text) for sent in sentences]):
+        i += length
+        if i > entity.start_char:
+            return sentences[ind]
 
 
 def search_wikidata(query: str) -> List[Dict]:
