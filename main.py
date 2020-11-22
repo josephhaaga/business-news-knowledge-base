4fc3197879b1cd23f68a8d9e14b792004843b5d3
Joseph Haaga <haaga.joe@gmail.com>
2020-11-22 17:20:14 -0500

    using entity type in Wikidata query

--- main.py
+++ main.py
@@ -1,105 +1,32 @@
-from typing import Dict
-from typing import List
-from typing import Sequence
+import operator
 
-from bs4 import BeautifulSoup
-from newspaper import Article
-import requests
 import spacy
-from spacy import displacy
 
-
-nlp = spacy.load("en_core_web_md")
+from utils import download_article
+from utils import get_entity_mentions
+from utils import get_mention_of_entity
+from utils import search_wikidata
 
 
 def main() -> int:
     URL = "https://www.cnbc.com/2020/09/22/palantir-says-it-expects-42percent-revenue-growth-this-year-to-1point06-billion.html"
-    article = download_article(URL)
-    doc = nlp(article.text)
-    doc.user_data["title"] = article.title
-    # initial = displacy.serve(doc, style="ent")
 
-    entities = [
-        o
-        for o in doc.ents
-        if o.label_ in ["ORG", "PERSON", "GPE", "WORK_OF_ART", "FAC"]
-    ]
-    print(f"Extracted {len(entities)} entities")
+    doc = download_article(URL)
+    entity_mentions = get_entity_mentions(doc)
+    print(f"Extracted {len(entity_mentions)} entities")
 
-    consolidated_entities = {
-        entity.text: [mention for mention in entities if mention.text == entity.text]
-        for entity in entities
-    }
-    for entity, mentions in consolidated_entities.items():
+    for entity, mentions in entity_mentions.items():
         mention_spans = [get_mention_of_entity(mention) for mention in mentions]
         composite_mention = " ".join([mention.text for mention in mention_spans])
         print(f"Searching for {entity} using the following context:\n{composite_mention}")
-        possible_matches = search_wikidata(entity)
-        # TODO: use SPARQL query to find matches that are of the expected NER type (or subtype)
-        if len(possible_matches) < 1:
-            print(f"Wikidata has no match for {entity}")
-            continue
-        pos_tag_description = nlp(
-            spacy.explain(mentions[0].label_)
-        )
-        results = []
-        for match in possible_matches:
-            # use a Wikidata library to grab metadata/categories (description embeddings don't work)
-            if len(match["description"]) > 0:
-                score = nlp(match["description"]).similarity(pos_tag_description)
-                results += [(match["id"], score)]
-                print(f"{match['title']}: {match['description']}")
+
+        # pos_tag_description = spacy.explain(mentions[0].label_)
+        _ = [mention.label_ for mention in mentions]
+        labels_and_counts = {i: _.count(i) for i in set(_)}
+        most_frequent_label = max(labels_and_counts.items(), key=operator.itemgetter(1))[0]
+
+        potential_matches = search_wikidata(entity, most_frequent_label)
         breakpoint()
-        results.sort(key=lambda x: x[1], reverse=True)
-        top_result, *_ = results
-        (best_match,) = [
-            match for match in possible_matches if match["id"] == top_result[0]
-        ]
-        print(
-            f"{entity} is likely {best_match['id']} - {best_match['title']}: {best_match['description']} with confidence {top_result[1]}"
-        )
-        print()
-        # TODO: add to KnowledgeBase
-    breakpoint()
-    # TODO: retrain the model/KnowledgeBase
-
-    # resolve to entities on wikidata
-    for org in orgs:
-        wikidata_matches = search_wikidata(org)
-        print(f"{org} returned {len(wikidata_matches)} Wikidata matches:")
-        for match in wikidata_matches:
-            print(f"  {match['id']} - {match['title']}: {match['description']}")
-
-
-def download_article(url: str) -> Article:
-    article = Article(url)
-    article.download()
-    article.parse()
-    return article
-
-
-def get_mention_of_entity(entity):
-    total = 0
-    for sent in entity.doc.sents:
-        total += len(sent.text)
-        if total > entity.start_char:
-            return sent
-
-
-def search_wikidata(query: str) -> List[Dict]:
-    page = requests.get(f"https://www.wikidata.org/w/index.php?search={query}")
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find_all("li", class_="mw-search-result")
-    return [
-        {
-            "title": result.find("span", class_="wb-itemlink-label").text,
-            "id": result.find("span", class_="wb-itemlink-id")
-            .text.lstrip("(")
-            .rstrip(")"),
-            "description": result.find("span", class_="wb-itemlink-description").text,
-        }
-        for result in results
-    ]
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,81 @@
+from typing import Dict
+from typing import List
+from typing import Sequence
+
+from newspaper import Article
+import requests
+import spacy
+from spacy import displacy
+
+
+nlp = spacy.load("en_core_web_md")
+
+
+def download_article(url: str) -> spacy.tokens.doc.Doc:
+    article = Article(url)
+    article.download()
+    article.parse()
+    doc = nlp(article.text)
+    doc.user_data["title"] = article.title
+    # initial = displacy.serve(doc, style="ent")
+    return doc
+
+
+def get_entity_mentions(doc) -> Dict[str, spacy.tokens.span.Span]:
+    entities = [
+        o
+        for o in doc.ents
+        if o.label_ in ["ORG", "PERSON", "GPE", "WORK_OF_ART", "FAC"]
+    ]
+    mentions_grouped_by_entity = {
+        entity.text: [mention for mention in entities if mention.text == entity.text]
+        for entity in entities
+    }
+    return mentions_grouped_by_entity
+
+
+# TODO: document what this does; it looks like it gets every mention of an entity in the source doc?
+def get_mention_of_entity(entity):
+    total = 0
+    for sent in entity.doc.sents:
+        total += len(sent.text)
+        if total > entity.start_char:
+            return sent
+
+
+QID_FOR_POS_TAG = {
+    "ORG": "Q43229",
+    "PERSON": "Q5",
+    "GPE": "Q15642541",
+    "WORK_OF_ART": "Q15621286",
+    "FAC": "Q811979"
+}
+
+
+def search_wikidata(entity_name: str, entity_pos_tag: str) -> List[Dict]:
+    """Search Wikidata for entities named `entity_name`, whose type is a hyponym or match of `entity_type`.
+    """
+    url = 'https://query.wikidata.org/sparql'
+    QID = QID_FOR_POS_TAG[entity_pos_tag]
+    query = f"""
+SELECT ?entity ?entityLabel WHERE {{
+    SERVICE wikibase:mwapi {{
+        bd:serviceParam wikibase:endpoint "www.wikidata.org";
+        wikibase:api "EntitySearch";
+        mwapi:search "{entity_name}";
+        mwapi:language "en".
+        ?entity wikibase:apiOutputItem mwapi:item.
+    }}
+    ?entity wdt:P31/wdt:P279* wd:{QID} .
+    SERVICE wikibase:label {{ #BabelRainbow
+        bd:serviceParam wikibase:language "[AUTO_LANGUAGE],fr,ar,be,bg,bn,ca,cs,da,de,el,en,es,et,fa,fi,he,hi,hu,hy,id,it,ja,jv,ko,nb,nl,eo,pa,pl,pt,ro,ru,sh,sk,sr,sv,sw,te,th,tr,uk,yue,vec,vi,zh"
+    }}
+}}
+LIMIT 100
+    """
+    r = requests.get(url, params = {'format': 'json', 'query': query})
+    data = r.json()
+    return data['results']['bindings']
+
+
+
